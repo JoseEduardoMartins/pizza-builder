8fe9d3471587cc62ea6ba6640654a31d42551bc3
Eduardo <m4rt1ns.jose@gmail.com>
2024-07-19 13:47:35 -0300

    renamed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { OrderProvider } from "@/contexts/OrderContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Monte sua pizza",
-  description: "Monte sua pizza",
+  title: "Pizzaria Voors",
+  description: "Pizzaria Voors",
 };
 
 export default function RootLayout({
